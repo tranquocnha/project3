048ded1b6ac9c5e60c1143f7eeaca6300984c4aa
trunghau4869 <trunghau4869@gmail.com>
2022-11-28 15:37:24 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-setx POSTGRES_USERNAME myusername
-setx POSTGRES_PASSWORD mypassword
-setx POSTGRES_HOST database-3.c6dtc3ezn7cv.us-east-1.rds.amazonaws.com
-setx POSTGRES_DB postgres
-setx AWS_BUCKET nhatq
-setx AWS_REGION us-east-1
-setx AWS_PROFILE default
-setx JWT_SECRET testing
-setx URL http://localhost:8100
+export POSTGRES_USERNAME myusername
+export POSTGRES_PASSWORD mypassword
+export POSTGRES_HOST database-3.c6dtc3ezn7cv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB postgres
+export AWS_BUCKET nhatq
+export AWS_REGION us-east-1
+export AWS_PROFILE default
+export JWT_SECRET testing
+export URL http://localhost:8100
 
