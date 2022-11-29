8c2e247bc71ffe68aab052064117f40286f9d96c
trunghau4869 <trunghau4869@gmail.com>
2022-11-29 13:10:14 +0700

    update

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,3 +8,8 @@ kubectl apply -f  user-deploy.yml
 kubectl apply -f  feed-deploy.yml
 kubectl apply -f  reverseproxy-deploy.yml
 kubectl apply -f  frontend-deploy.yml
+
+
+kubectl expose deployment frontend --type=LoadBalancer --name=publicfrontend
+
+kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreverseproxy
