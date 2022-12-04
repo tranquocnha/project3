ac64d0e9a7acbdee02ca9b08f52f7426e5ee7f86
trunghau4869 <trunghau4869@gmail.com>
2022-12-04 18:37:11 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME myusername
-export POSTGRES_PASSWORD mypassword
-export POSTGRES_HOST database-3.c6dtc3ezn7cv.us-east-1.rds.amazonaws.com
-export POSTGRES_DB postgres
-export AWS_BUCKET nhatq
-export AWS_REGION us-east-1
-export AWS_PROFILE default
-export JWT_SECRET testing
-export URL http://localhost:8100
+export POSTGRES_USERNAME= myusername
+export POSTGRES_PASSWORD= mypassword
+export POSTGRES_HOST= database-1.c6dtc3ezn7cv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB= postgres
+export AWS_BUCKET= nhatq
+export AWS_REGION= us-east-1
+export AWS_PROFILE= default
+export JWT_SECRET= testing
+export URL= http://localhost:8100
 
--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,8 +7,8 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
-  'aws_access_key_id': "ASIA3LV35GSQB3QDNLXD",
-  'aws_secret_access_key': "qglotcoUjDqeKrWP9m3n9kiEfAX4dukwSA89RGG6",
+  'aws_access_key_id': "ASIA3LV35GSQA746E7F5",
+  'aws_secret_access_key': "EIMsDtWmdWG+lJDZocyLTuGFhbq60o6BQSztoJXT",
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
